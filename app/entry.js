4341730723be533b651b1987c4236098313e57fe
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-03-21 18:21:23 -0700

    finished index.html and entry.js

--- app/entry.js
+++ app/entry.js
@@ -1,4 +1,63 @@
 'use strict';
 
+require('./scss/reset.scss');
+require('./scss/main.scss');
+
 const angular = require('angular');
 const cowsay = require('cowsay-browser');
+
+const mooMooApp = angular.module('mooMooApp', []);
+
+mooMooApp.controller('CowsayController', ['$log', CowsayController]);
+
+function CowsayController($log) {
+  $log.debug(CowsayController);
+
+  this.title = 'Cowsay Whaaa?';
+  this.history = [];
+
+  cowsay.list((err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.current = cowfiles['default'];
+  });
+
+  this.update = function(input) {
+    $log.debug('cowsayCtrl.update');
+    return cowsay.say( {text: input || 'Fricken Moo', f: this.current} );
+  };
+
+  this.speak = function(input) {
+    $log.debug('cowsayCtrl.speak');
+
+    this.spoken = this.update(input);
+    this.history.push(this.spoken);
+  };
+
+  this.undo = function() {
+    $log.debug('cowsayCtrl.undo');
+
+    this.history.pop();
+    this.spoken = this.history.pop() || '';
+  };
+}
+
+mooMooApp.controller('NavController', ['$log', NavController]);
+
+function NavController($log) {
+  $log.debug(NavController);
+
+  this.routes = [
+    {
+      name: 'Home',
+      url: '/home'
+    },
+    {
+      name: 'Cow Creator',
+      url: '/cow-creator'
+    },
+    {
+      name: 'Account',
+      url: '/account'
+    }
+  ];
+}
