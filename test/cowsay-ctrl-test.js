4f8430e8187be7026774d341d0910805c239bb32
Jeremiah <jeremiaht.walters@gmail.com>
2017-03-22 21:48:33 -0700

    test passing bonus started

--- test/cowsay-ctrl-test.js
+++ test/cowsay-ctrl-test.js
@@ -12,10 +12,48 @@ describe('Cowsay Controller', function(){
       this.cowsayCtrl = new $controller('CowsayController');
     });
   });
-  
+
   describe('initial properties', () => {
     it('title property should equal Cow Creator', () => {
       expect(this.cowsayCtrl.title).toBe('Cow Creator');
+    });
+
+    it('history property should be an empty array', () => {
+      expect(Array.isArray(this.cowsayCtrl.history)).toBe(true);
+    });
+
+    it('list of cowfiles should show proper cowfiles', () => {
+      cowsay.list((err, list) => {
+        expect(this.cowsayCtrl.cowfiles).toEqual(list);
+        expect(this.cowsayCtrl.current).toEqual(list[0]);
+      });
+    });
+  });
+
+  describe('#update', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      let result = this.cowsayCtrl.update('testing');
+      expect(result).toEqual(expected);
+    });
+  });
+  describe('#speak', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      this.cowsayCtrl.speak('testing');
+      expect(this.cowsayCtrl.spoken).toEqual(expected);
+      expect(this.cowsayCtrl.history[0]).toEqual(expected);
+    });
+  });
+
+  describe('#undo', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      this.cowsayCtrl.speak('testing');
+      this.cowsayCtrl.speak('testing again');
+      this.cowsayCtrl.undo();
+      expect(this.cowsayCtrl.spoken).toEqual(expected);
+      expect(this.cowsayCtrl.history.length).toEqual(0);
     });
   });
 });
